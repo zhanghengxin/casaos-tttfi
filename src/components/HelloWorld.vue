c91f61361c5783eecf596ec9c49aa969beb4a2d3
zhanghengxin <ezreal.ice@icloud.com>
2022-11-09 21:17:29 +0800

    wip:2022.11.09 night

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,23 @@
-<script setup>
+<script setup xmlns="http://www.w3.org/1999/html">
 import { ref } from 'vue'
+// ref:
+// import SvgIcon from '@jamescoyle/vue-icon'
+// import { mdiCalendarFilterOutline } from '@mdi/js'
 
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
+components: {
+  // SvgIcon
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <v-btn stacked size="x-large" @click.prevent="getEvent">
+    when
+    <v-icon
+        end
+        icon="mdi-cancel"
+    ></v-icon>
+  </v-btn>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// ref: https://next.vuetifyjs.com/en/getting-started/installation/#manual-steps
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
